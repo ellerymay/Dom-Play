af53efb31860ebc6a094a0d20a6f29895a1e1fb2
ellerymay <emcsarquilla@gmail.com>
2022-03-04 12:39:54 -0800

    update for portal

--- js/dom.js
+++ js/dom.js
@@ -1,25 +1,20 @@
-var spans = document.querySelectorAll("#clickSpans span");
-
-for(const mySpan of spans){
-    mySpan.addEventListener("click",function(event){
+const spans = document.querySelectorAll('div#play span');
+    console.log(spans);
+    for (const mySpan of spans) {
+      mySpan.addEventListener('click', function(ev){
         //alert(mySpan.dataset.actor);
         highlightActor(mySpan.dataset.actor);
-    });
-}
+      });
+    }
 
-
-//console.log(spans);
-
-function highlightActor(myActor){
-
-for(const mySpan of spans){
-
-    if(mySpan.dataset.actor == myActor){//current actor highlighted
-        mySpan.style.backgroundColor = 'red';
-        mySpan.style.fontWeight = 'bold';
-    }else{//not current actor, no highlight
-        mySpan.style.backgroundColor = 'white';
-        mySpan.style.fontWeight = 'normal';
-}
-}
-}
+    function highlightActor(actor){
+      for (const mySpan of spans) {
+        if (actor == mySpan.dataset.actor){
+        mySpan.style.backgroundColor = "yellow";
+      }
+      else {
+        mySpan.style.backgroundColor = "white";
+      }
+      }
+    }
+    //document.querySelector('span.hamlet').style.backgroundColor = "yellow";
